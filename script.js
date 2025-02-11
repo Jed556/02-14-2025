5f31435930eed2e27b3102378fc00b5bc45a9128
Jed556 <jguiriba11@gmail.com>
2025-02-11 22:08:26 +0800

    feat: Make the envelope clickable instead

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     const envelope = $('#envelope');
-    const btnOpen = $("#open");
-    const btnReset = $("#reset");
 
     envelope.on('click', function () {
         if (envelope.hasClass("open")) {
@@ -10,20 +8,13 @@ $(document).ready(function () {
             envelope.removeClass("close").addClass("open");
         }
     });
-    btnOpen.on('click', open);
-    btnReset.on('click', close);
-
-    function open() {
-        envelope.removeClass("close").addClass("open");
-    }
-
-    function close() {
-        envelope.removeClass("open").addClass("close");
-    }
 
     $(document).on('mousemove', function (e) {
         const x = (window.innerWidth / 2 - e.pageX) / 20;
         const y = (window.innerHeight / 2 - e.pageY) / 20;
-        envelope.css('transform', `rotateY(${x}deg) rotateX(-${y}deg)`);
+        envelope.css('transform', `rotateY(${x}deg) rotateX(${y}deg)`);
     });
+
+    // Trigger the scale down animation on page load
+    envelope.addClass('animate');
 });
