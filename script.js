de725d5eb34f210e95c7dce569391a69e2fd5d14
Jed556 <jguiriba11@gmail.com>
2025-02-11 22:27:24 +0800

    style: Add fonts and stickers

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 $(document).ready(function () {
     const envelope = $('#envelope');
+    const introText = $('#intro-text');
+    const introMessage = $('#intro-message');
+    const sticker = $('#sticker');
+    const messages = ["Hi Babyyyy!", "Happy Valentines!", "..."];
+    let messageIndex = 0;
 
-    envelope.on('click', function () {
-        if (envelope.hasClass("open")) {
-            envelope.removeClass("open").addClass("close");
+    function showNextMessage() {
+        if (messageIndex < messages.length) {
+            introMessage.text(messages[messageIndex]);
+            introText.css('animation', 'none');
+            introText[0].offsetHeight; // Trigger reflow
+            introText.css('animation', '');
+            if (messageIndex === 1) {
+                sticker.show();
+            } else {
+                sticker.hide();
+            }
+            messageIndex++;
+            setTimeout(showNextMessage, 3000);
         } else {
-            envelope.removeClass("close").addClass("open");
+            introText.hide();
+            envelope.addClass('animate');
+            enableEnvelopeClick();
         }
-    });
+    }
+
+    function enableEnvelopeClick() {
+        envelope.on('click', function () {
+            if (envelope.hasClass("open")) {
+                envelope.removeClass("open").addClass("close");
+            } else {
+                envelope.removeClass("close").addClass("open");
+            }
+        });
+    }
+
+    showNextMessage();
 
     $(document).on('mousemove', function (e) {
         const x = (window.innerWidth / 2 - e.pageX) / 20;
         const y = (window.innerHeight / 2 - e.pageY) / 20;
         envelope.css('transform', `rotateY(${x}deg) rotateX(${y}deg)`);
     });
-
-    // Trigger the scale down animation on page load
-    envelope.addClass('animate');
 });
